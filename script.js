332a45466f1d657ed704cf5d19ddf1aefb8225b8
ASHRUTHA G <137149491+ASHRUTHA24@users.noreply.github.com>
2023-08-12 19:18:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let slideIndex = 0;
+
+function showSlide(index) {
+    const slides = document.getElementsByClassName('slide');
+    
+    if (index >= slides.length) {
+        slideIndex = 0;
+    } else if (index < 0) {
+        slideIndex = slides.length - 1;
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+
+    slides[slideIndex].style.display = 'block';
+}
+
+function changeSlide(n) {
+    slideIndex += n;
+    showSlide(slideIndex);
+}
+
+showSlide(slideIndex);
